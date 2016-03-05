11c83f95b8bad583381da39792490fb3671783b3
Nitai Bezerra <nitaibezerra@gmail.com>
2016-03-05 19:06:02 -0300

    Incluindo navegação nas páginas de convênios

--- download-dados.py
+++ download-dados.py
@@ -4,20 +4,26 @@ import json
 import os
 import urlparse
 
-pagina = requests.get("http://api.convenios.gov.br/siconv/v1/consulta/convenios.json")
-json_pagina = json.loads(pagina.content)
-urls = [convenio["href"] for convenio in json_pagina["convenios"]]
+url_pagina = 'http://api.convenios.gov.br/siconv/v1/consulta/convenios.json'
 
-if not os.path.exists('dados'):
-    print u'Criando diretório de dados'
-    os.mkdir('dados')
+while True:
+    pagina = requests.get(url_pagina)
+    json_pagina = json.loads(pagina.content)
+    urls = [convenio["href"] for convenio in json_pagina["convenios"]]
 
-for url in urls:
-    convenio = requests.get(url, headers={'accept': 'application/json'})
-    if convenio.ok:
-        filename = os.path.basename(urlparse.urlparse(url).path)
-        with open('dados/convenio-id-%s.json' % filename, 'w') as f:
-            print u'Gravando arquivo do convênio de id: %s' % filename
-            f.write(convenio.content)
-    else:
-        print 'ERRO ao gravar arquivo do convênio de id: %s' % filename
+    if not os.path.exists('dados'):
+        print u'Criando diretório de dados'
+        os.mkdir('dados')
+
+    for url in urls:
+        convenio = requests.get(url, headers={'accept': 'application/json'})
+        if convenio.ok:
+            filename = os.path.basename(urlparse.urlparse(url).path)
+            with open('dados/convenio-id-%s.json' % filename, 'w') as f:
+                print u'Gravando arquivo do convênio de id: %s' % filename
+                f.write(convenio.content)
+        else:
+            print 'ERRO ao gravar arquivo do convênio de id: %s' % filename
+
+    #Baixando próxima página de convênios. Cada página contém 500 convênios
+    url_pagina = json_pagina["metadados"]["proximos"]
